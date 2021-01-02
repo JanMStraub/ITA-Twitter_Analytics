617be6735b0ffa9d4cd72b60b96f8eb7a45c9c02
Maximilian Schöneberger <maxschoeneberger@t-online.de>
2021-01-02 15:15:56 +0100

    fixed routing !

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import AboutProject from './AboutProject';
 import Mobile from './mobile_notifiy';
@@ -11,13 +11,13 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <div className="App">
           <Mobile />
           <Switch>
-            <Route path="/ITA-Twitter_Analytics" exact component={LandingPage} />
-            <Route path="/ITA-Twitter_Analytics/aboutproject" component={AboutProject} />
-            <Redirect to="/ITA-Twitter_Analytics" />
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/aboutproject" component={AboutProject} />
+            <Redirect to="/" />
           </Switch>
         </div>
       </Router>
