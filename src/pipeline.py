79a583acf9ac05c5a59d135d6226fb40dff6f2b8
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-01-04 15:47:44 +0100

    removed remove_dupclivate_links

--- src/pipeline.py
+++ src/pipeline.py
@@ -26,6 +26,7 @@ def read_from_storage(filename):
 
     return tweets
 
+# Not used
 def remove_duplicate_links(extracted_links):
     """
     IN:
@@ -56,7 +57,7 @@ def get_links_from_tweet(tweets):
             extracted_links.append(re.findall(link_string, tweet))
     
 
-    return remove_duplicate_links(extracted_links)
+    return extracted_links
 
 def remove_numbers_and_links(tweets):
     """
@@ -111,48 +112,6 @@ def clean_tweets(trend_from_storage):
                 lemmatized_dict[t.lemma_] += X[0:X.shape[0], index].sum()
 
     return lemmatized_dict #, extracted_links
-
-# not quite finished yet
-def clean_tweets_twopointo(trend_from_storage):
-    """
-    IN:
-    trend_from_storage (string): one trend in the form "<trend>.json"
-    OUT:
-    lemmatized_dict (dict): preprocessed data dict for that trend, structure: {"word": <word count>, ...}
-    """
-
-    # load nltk stopwords
-    nltk.download('stopwords')
-    # load NLP for tokenisation/lemmatization
-    nlp = spacy.load('de_core_news_sm')
-    nlp.disable_pipes('tagger', 'parser', 'ner')
-
-    tweets = read_from_storage(trend_from_storage)
-    # remove numbers
-    for index in range(len(tweets)):
-        tweets[index] = re.sub(r'[\d]', '', tweets[index])
-
-    lemmatized_dict = {}
-    sorted_list = []
-
-    for line in tweets:
-        doc = nlp(line)
-        # token_list = [token for token in doc]
-        filtered_tokens = [token for token in doc if not token.is_stop]
-        lemmas = [token.lemma_ for token in filtered_tokens]
-        # might change to a list comprehension
-        while(" " in lemmas):
-            lemmas.remove(" ")
-        sorted_list.append(lemmas)
-    
-    for token_list in sorted_list:
-        for token in token_list:          
-            if not lemmatized_dict.get(token):
-                lemmatized_dict[token] = 1
-            else:
-                lemmatized_dict[token] += 1
-
-    return lemmatized_dict
 
 if __name__ == "__main__":
 
