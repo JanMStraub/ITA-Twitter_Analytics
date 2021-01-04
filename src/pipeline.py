fe9bdefc262b0685445883f96bce52b67ca9d145
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-01-04 12:58:22 +0100

    update pipeline to new version

--- src/pipeline.py
+++ src/pipeline.py
@@ -14,6 +14,7 @@ def read_from_storage(filename):
     OUT:
     tweets (list): text of all tweets of the trend
     """
+
     current_dir = os.path.dirname(os.path.abspath(__file__))
     path = current_dir + '/../storage/jsons/' + filename
     tweets = []
@@ -22,6 +23,56 @@ def read_from_storage(filename):
         tweets_json = json.load(file)
         for tweet in tweets_json:
             tweets.append(tweet["text"])
+
+    return tweets
+
+
+def remove_duplicate_links(extracted_links):
+    """
+    IN:
+    extracted_links (list): list containing all links in on trend
+    OUT:
+    extracted_links_filterd (list): list containing all links in on trend but duplicates are removed
+    """
+
+    extracted_links_filterd = []
+
+    [extracted_links_filterd.append(
+        link) for link in extracted_links if link not in extracted_links_filterd]
+
+    return extracted_links_filterd
+
+
+def get_links_from_tweet(tweets):
+    """
+    IN:
+    tweets (list): text of all tweets of the trend
+    OUT:
+    extracted_links (list): list containing all links in on trend
+    """
+
+    extracted_links = []
+
+    link_string = "(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)"
+    for tweet in tweets:
+        if re.search(link_string, tweet):
+            extracted_links.append(re.findall(link_string, tweet))
+
+    return remove_duplicate_links(extracted_links)
+
+
+def remove_numbers_and_links(tweets):
+    """
+    IN:
+    tweets (list): text of all tweets of the trend
+    OUT:
+    tweets (list): text of all tweets of the trend
+    """
+
+    for index in range(len(tweets)):
+        tweets[index] = re.sub(r'[\d]', '', tweets[index])
+        tweets[index] = re.sub(
+            r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', tweets[index])
 
     return tweets
 
@@ -41,27 +92,77 @@ def clean_tweets(trend_from_storage):
     nlp.disable_pipes('tagger', 'parser', 'ner')
 
     tweets = read_from_storage(trend_from_storage)
-    for index in range(len(tweets)):
-        tweets[index] = re.sub(r'[\d]', '', tweets[index])
+
+    # Get all links in tweets
+    extracted_links = get_links_from_tweet(tweets)
+
+    # Remove all numbers and links
+    tweets = remove_numbers_and_links(tweets)
 
     german_stop_words = stopwords.words('german')
-    vectorizer = CountVectorizer(analyzer="word", lowercase=True, stop_words=german_stop_words)
-    vectorizer.fit_transform(tweets)
+    vectorizer = CountVectorizer(
+        analyzer="word", lowercase=True, stop_words=german_stop_words)
+    X = vectorizer.fit_transform(tweets).toarray()
 
     lemmatized_dict = {}
     sorted_list = dict(vectorizer.vocabulary_.items())
 
     for string in list(sorted_list.keys()):
+        index = sorted_list[string]
         for t in nlp.tokenizer(string):
             if t.lemma_ not in lemmatized_dict:
-                lemmatized_dict[t.lemma_] = sorted_list[string]
+                lemmatized_dict[t.lemma_] = X[0:X.shape[0], index].sum()
             else:
-                lemmatized_dict[t.lemma_] += sorted_list[string]
+                lemmatized_dict[t.lemma_] += X[0:X.shape[0], index].sum()
+
+    return lemmatized_dict, extracted_links
+
+# not quite finished yet
+
+
+def clean_tweets_twopointo(trend_from_storage):
+    """
+    IN:
+    trend_from_storage (string): one trend in the form "<trend>.json"
+    OUT:
+    lemmatized_dict (dict): preprocessed data dict for that trend, structure: {"word": <word count>, ...}
+    """
+
+    # load nltk stopwords
+    nltk.download('stopwords')
+    # load NLP for tokenisation/lemmatization
+    nlp = spacy.load('de_core_news_sm')
+    nlp.disable_pipes('tagger', 'parser', 'ner')
+
+    tweets = read_from_storage(trend_from_storage)
+    # remove numbers
+    for index in range(len(tweets)):
+        tweets[index] = re.sub(r'[\d]', '', tweets[index])
+
+    lemmatized_dict = {}
+    sorted_list = []
+
+    for line in tweets:
+        doc = nlp(line)
+        # token_list = [token for token in doc]
+        filtered_tokens = [token for token in doc if not token.is_stop]
+        lemmas = [token.lemma_ for token in filtered_tokens]
+        # might change to a list comprehension
+        while(" " in lemmas):
+            lemmas.remove(" ")
+        sorted_list.append(lemmas)
+
+    for token_list in sorted_list:
+        for token in token_list:
+            if not lemmatized_dict.get(token):
+                lemmatized_dict[token] = 1
+            else:
+                lemmatized_dict[token] += 1
 
     return lemmatized_dict
 
 
 if __name__ == "__main__":
 
-    clean_tweets("#Formel1.json")
+    print(clean_tweets("Vettel.json"))
     print("\nYou are doing great! :)")  # Motivational Message
