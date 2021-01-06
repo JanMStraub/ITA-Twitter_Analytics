5a0c7d245ee977fc033c81516f5c951346db0a2d
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-01-06 10:14:35 +0100

    added model_training

--- src/build_topic_modeling.py
+++ src/build_topic_modeling.py
@@ -1,7 +1,9 @@
 import os
 import gensim.corpora as corpora
+import gensim.models as models
 
 from pipeline import clean_tweets
+from pprint import pprint
 
 # get all trend filenames from storage
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -32,13 +34,29 @@ def preparing_data_for_LDA(word_list):
     # Term Document Frequency
     corpus = [id2word.doc2bow(text) for text in texts]
 
-    print(corpus)
+    return corpus, id2word
+
+
+def model_training(corpus, id2word):
+    # number of topics
+    num_topics = 10
+
+    # Build LDA model
+    lda_model = models.LdaMulticore(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=num_topics)
+
+    pprint(lda_model.print_topics())
+    doc_lda = lda_model[corpus]
+
 
 if __name__ == '__main__':
 
     for trend in trends[25:26]:
         data = clean_tweets(trend)
         # print(convert_dict_to_list(data))
-        preparing_data_for_LDA(convert_dict_to_list(data))
+        list_data = convert_dict_to_list(data)
+        corpus_data, id2word_data = preparing_data_for_LDA(list_data)
+        model_training(corpus_data, id2word_data)
 
 print("You are doing great! :)")
