92aa70e9aee3dc52eab494a901aa1a176a878ea0
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-01-30 19:15:47 +0100

    pipeline redone

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,7 +5,7 @@ import re
 import spacy
 
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+from gensim.models.phrases import Phrases, Phraser
 
 
 def read_from_storage(filename):
@@ -111,10 +111,62 @@ def clean_tweets(trend_from_storage):
     additional_stopwords = ["rt", "lt"]
     german_stop_words.extend(additional_stopwords)
 
+    """
+    for tweet in tweets:
+        re.sub  # lower case and remove non-alphabetic characters
+        nlp
+        stopwords remove
+        one char tokens
+        empty tokens...
+        cleaned_tweets.append()
+        bigrams
+
+        -> lemmatized dict
+    """
+
+    lemmatized_dict = {}
+
+    for tweet in tweets:
+        # lower case and remove non-alphabetic characters
+        tweet = re.sub("[/']", '', re.sub("[^A-Za-z\d']+", ' ', str(tweet))).lower()
+        tweet = nlp.tokenizer(tweet)
+        # removing the stopwords
+        tweet = [str(word) for word in tweet if str(word) not in german_stop_words]
+        # removing one char tokens
+        tweet = [word for word in tweet if (len(word) > 1)]
+        # removing empty tokens
+        tweet = [word for word in tweet if (len(word) != 0)]
+        # removing empty list
+        if len(tweet) > 0:
+            
+            tweet_stream = [tweet]
+            print(tweet)
+            # define the phraser for bi-gram creation#
+            phrases = Phrases(tweet_stream, threshold=2)
+            bigram = Phraser(phrases)
+            new_lines = bigram[tweet]
+            print(new_lines)
+
+
+    """
+
+    sent = [] # list of all words
+    sent = quotes
+
+    # for quote in quotes:
+    #     for word in quote:
+    #         sent.append(word)
+
+
+    phrases = Phrases(sent, min_count=10, threshold=2) #define the phraser for bi-gram creation#
+    bigram = Phraser(phrases)
+
+    new_lines = bigram[sent] # transformed the lines
+
     vectorizer = CountVectorizer(analyzer="word", lowercase=True, ngram_range=(1, 2), stop_words=german_stop_words)
     X = vectorizer.fit_transform(tweets).toarray()
 
-    lemmatized_dict = {}
+    
     sorted_list = dict(vectorizer.vocabulary_.items())
 
     for string in list(sorted_list.keys()):
@@ -123,6 +175,7 @@ def clean_tweets(trend_from_storage):
         token.append(nlp.tokenizer(string))
         print(token)
 
+    
     for string in list(sorted_list.keys()):
         index = sorted_list[string]
         for t in nlp.tokenizer(string):
@@ -130,6 +183,7 @@ def clean_tweets(trend_from_storage):
                 lemmatized_dict[t.lemma_] = X[0:X.shape[0], index].sum()
             else:
                 lemmatized_dict[t.lemma_] += X[0:X.shape[0], index].sum()
+    """
 
     return lemmatized_dict
 
